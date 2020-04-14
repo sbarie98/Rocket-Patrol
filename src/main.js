90fb73c575fb2c5440adb297c5530b105f3078f0
Simone Barie <sbarie@ucsc.edu>
2020-04-13 21:05:55 -0700

    third commit

--- src/main.js
+++ src/main.js
@@ -7,3 +7,12 @@ let config = {
 
 let game = new Phaser.Game(config);
 
+// define game settings
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000
+}
+
+// reserve keyboard vars
+let keyF, keyLEFT, keyRIGHT;
+
