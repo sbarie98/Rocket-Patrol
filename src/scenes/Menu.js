df4af46372b138568f8a6fdf2cb06b8d9f372206
Simone Barie <sbarie@ucsc.edu>
2020-04-14 19:04:24 -0700

    third commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,9 +5,9 @@ class Menu extends Phaser.Scene {
 
     preload() {
         // load audio
-        this.load.audio('sfx_select', './assets/selectBeep.wav');
-        this.load.audio('sfx_explosion', './assets/popBalloon.wav');
-        this.load.audio('sfx_rocket', './assets/dartWoosh.wav');
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
     }
 
     create() {
@@ -25,7 +25,7 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
         }
 
-        this.add.text(20,20, "Balloon Patrol Menu");
+        this.add.text(20,20, "Rocket Patrol Menu");
         //this.scene.start("playScene");
 
         // show menu text
@@ -33,7 +33,7 @@ class Menu extends Phaser.Scene {
         let centerY = game.config.height/2
         let textSpacer = 64;
 
-        this.add.text(centerX, centerY- textSpacer, 'BALLOON PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY- textSpacer, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(centerX, centerY, 'Use ←→ arrows to move & (F) to Fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,12 +5,12 @@ class Play extends Phaser.Scene {
 
     preload() {
         // load image/title sprites
-        this.load.image('rocket', './assets/dart1.png');
-        this.load.image('spaceship', './assets/balloonAnimal1.png');
-        this.load.image('starfield', './assets/sky.png');
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
 
         // load spritesheet
-        this.load.spritesheet('explosion', './assets/balloonPop1.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 3});
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 3});
     }
 
     create() {
